1a1dc858398a04f641817ab96d3228468dca2b14
pnguye35 <pnguye35@ford.com>
2020-06-12 16:45:03 +0000

    adding the circuit breaker

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,29 @@
 package io.pivotal.pal.tracker.allocations;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final String registrationServerEndpoint;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
-
+    @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        projectsCache.put(projectId, project);
+        return project;
+    }
+
+    public ProjectInfo getProjectFromCache (long projectId) {
+        return projectsCache.get(projectId);
     }
 }
